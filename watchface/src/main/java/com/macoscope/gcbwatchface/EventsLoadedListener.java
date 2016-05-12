6b465debe9e62ba72767b8c3fbaaad156ef2c3fd
Tomasz Kulikowski <tomasz@macoscope.net>
2016-05-12 11:31:45 +0200

    Watch: removed quering WearableCalendarProvider

--- watchface/src/main/java/com/macoscope/gcbwatchface/EventsLoadedListener.java
+++
@@ -1,9 +0,0 @@
-package com.macoscope.gcbwatchface;
-
-
-import java.util.List;
-
-public interface EventsLoadedListener {
-    void onEventsLoaded(List<EventModel> events);
-    void onNoEventsLoaded();
-}
